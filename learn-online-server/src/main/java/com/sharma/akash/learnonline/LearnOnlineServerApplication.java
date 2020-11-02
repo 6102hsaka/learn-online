59d03c54c70358d72faeab7bbaa02d7d1575ad06
Akash Sharma <akashsharma15105@gmail.com>
2020-11-02 23:40:08 +0530

    Project Initial Setup

---
+++ learn-online-server/src/main/java/com/sharma/akash/learnonline/LearnOnlineServerApplication.java
@@ -0,0 +1,13 @@
+package com.sharma.akash.learnonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOnlineServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOnlineServerApplication.class, args);
+	}
+
+}
